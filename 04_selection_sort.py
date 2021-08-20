3e5086fcdd76fe23d5afe0eb14a4523db7a48953
jffLuiz <jeffersondl94@gmail.com>
2021-08-20 23:28:36 +0000

    .

--- 04_selection_sort.py
+++ 04_selection_sort.py
@@ -36,26 +36,26 @@ def selection_sort(lista):
 #pos_sel: 0 (onde esta o 88)
 #pos_menor: 1 (onde esta o 44)
 #j: 2 (onde esta o 33)
-# nums = [88, 44, 33, 0, 99, 55, 77, 22, 11, 66]
-# selection_sort(nums)
-# print(nums)
-# print(f"Embaralhado = Passadas: {passadas}, comparações: {comps}, trocas: {trocas}")
+nums = [88, 44, 33, 0, 99, 55, 77, 22, 11, 66]
+selection_sort(nums)
+print(nums)
+print(f"Embaralhado = Passadas: {passadas}, comparações: {comps}, trocas: {trocas}")
 
-# #não é o pior caso
-# nums = [99,88,77,66,55,44,33,22,11,0]
-# selection_sort(nums)
-# print(f"Não é o Pior caso = Passadas: {passadas}, comparações: {comps}, trocas: {trocas}")
+#não é o pior caso
+nums = [99,88,77,66,55,44,33,22,11,0]
+selection_sort(nums)
+print(f"Não é o Pior caso = Passadas: {passadas}, comparações: {comps}, trocas: {trocas}")
 
-# #melhor caso
-# nums = [0,11,22,33,44,55,66,77,88,99]
-# selection_sort(nums)
-# print(f"Melhor caso = Passadas: {passadas}, comparações: {comps}, trocas: {trocas}")
+#melhor caso
+nums = [0,11,22,33,44,55,66,77,88,99]
+selection_sort(nums)
+print(f"Melhor caso = Passadas: {passadas}, comparações: {comps}, trocas: {trocas}")
 
-# nums=[99,0,11,22,33,44,55,66,77,88]
-# #não é o pior caso
-# nums=[99,0,11,22,33,44,55,66,77,88]
-# selection_sort(nums)
-# print(f"Pior caso = Passadas: {passadas}, comparações: {comps}, trocas: {trocas}")
+nums=[99,0,11,22,33,44,55,66,77,88]
+#não é o pior caso
+nums=[99,0,11,22,33,44,55,66,77,88]
+selection_sort(nums)
+print(f"Pior caso = Passadas: {passadas}, comparações: {comps}, trocas: {trocas}")
 
 ##################################################
 
