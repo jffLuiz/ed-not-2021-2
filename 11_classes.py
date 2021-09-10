bfae323b8d461a46aeb1b6f2ad18903d6092f8b0
Jefferson Luiz Araujo da Silva <79008632+jffLuiz@users.noreply.github.com>
2021-09-10 20:31:32 -0300

    aula20210910

--- 11_classes.py
+++ 11_classes.py
@@ -3,9 +3,9 @@
 class FormaGeometrica():
     #dados
     #quando pertencem a uma classe, ganham o nome de Atributos
-    base = 0
-    altura = 0
-    tipo = "R" #retangulo
+    #base = 0
+    #altura = 0
+    #tipo = "R" #retangulo
 
     #algoritmos
     #são representados por funções que, quando se encontram dentro de uma classe, ganham o nome de Métodos
@@ -13,26 +13,80 @@ class FormaGeometrica():
     #este metodo é executado quando um objeto é criado a partir de uma classe (construtor)
     def __init__(self, base, altura, tipo = "R"):
         #recebe os valores passados ao construtor e os armazena dentro dos atributos
+        #print(f"base: {base} ({type(base)}), altura: {base} ({type(altura)}),")
 
-        if base<=0:
+        if type(base) not in [int,float] or base<=0:
             raise Exception("a base deve ser maior que zero")
-        elif altura<=0:
+        elif type(altura) not in [int,float] or altura<=0:
             raise Exception("a altura deve ser maior que zero")
         elif tipo not in ["R","T","E"]:
             raise Exception("o tipo deve ser R, T ou E")
 
+        #ajustando o valor dos atributos para privado ( adicionar __ no inicio do nome)
+        self.__base = base
+        self.__altura = altura
+        self.__tipo = tipo
+    
+    #Getter é um método que possibilita saber o valor de um atributo privado do lado de fora da classe
 
-        self.base = base
-        self.altura = altura
-        self.tipo = tipo
+    def __get_base(self):
+        return self.__base
+    
+    #setter é um metodo que possibilita alterar o valor de um atributo privado inclusive do lado de fora da classe
+
+    def __set_base(self, valor):
+
+        if type(valor) not in [int,float] or valor<=0:
+            raise Exception(" * a base deve ser maior que zero")
+
+        self.__base = valor
+
+    #property "esconde" as funções getter e setter dentro do nome de um atributo, tornando mais simples a manipulação
+    base = property(__get_base, __set_base)
+
+    #essas linha começadas com @ são chamadas de "decorators"
+    #os decorators instruem o python a criar uma property com um getter e um setter na hora da execução
+    @property
+    def altura(self):   #getter para a propriedade chamada "altura"
+        return self.__altura
+    
+    @altura.setter
+    def altura(self, valor): #setter para a propriedade chamada "altura"
+        if type(valor) not in [int, float] or valor <=0 :
+            raise Exception(" ** a base deve ser maior que zero")   
+        
+        self.__altura = valor  
+    
+    
+
 
 ##########################################
 
-#criando objetos a partir da classe
+#criando objetos (instanciar) a partir da classe
 
 retangulo1 = FormaGeometrica(15,10, "R") #chama o __init__
-triangulo1 = FormaGeometrica(6, 9, "T")
+triangulo1 = FormaGeometrica(6.4, 9, "T")
 
-print(f"[retangulo1] base: {retangulo1.base}, altura: {retangulo1.altura}, tipo: {retangulo1.tipo}")
+#print(f"[retangulo1] base: {retangulo1.get_base()}")
 
-print(f"[triangulo1] base: {triangulo1.base}, altura: {triangulo1.altura}, tipo: {triangulo1.tipo}")
+#retangulo1.__base = 5 #nao funciona
+#retangulo1.set_base(9.2)
+retangulo1.base = 9  #vai executar o set_base da classe
+#print(f"[retangulo1] base: {retangulo1.get_base()}")
+#retangulo1.set_base(12.5)
+
+#retangulo1.__base = 0
+#triangulo1.__tipo = "yadayada"
+
+#problematico = FormaGeometrica(7.2, 5, "Q")
+
+print(f"[retangulo1] base: {retangulo1.base}")
+
+#print(f"[retangulo1] base: {retangulo1.__base}, altura: {retangulo1.__altura}, tipo: {retangulo1.__tipo}")
+
+#print(f"[triangulo1] base: {triangulo1.__base}, altura: {triangulo1.__altura}, tipo: {triangulo1.__tipo}")
+
+#print(f"[problematico] base: {problematico.base}, altura: {problematico.altura}, tipo: {problematico.tipo}")
+
+#print(f"[retangulo1] base: {retangulo1.get_base()}")
+
